c04ec2135befdad8afd7ccdd7438aa60944225da
Turing <188160956@qq.com>
2019-01-30 19:02:36 +0800

    1. 接口去除重复定义的方法

--- app/src/main/java/com/sqw/mvp_traditional/model/base/IBaseListView.java
+++ app/src/main/java/com/sqw/mvp_traditional/model/base/IBaseListView.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 /**
  * 列表视图页接口 基类 继承于IBaseView
- * 相较于父接口IBaseView 增加了 设置适配器 没有更多 两个方法
  */
 
 public interface IBaseListView<T> extends IBaseView<T> {
@@ -23,11 +22,6 @@ public interface IBaseListView<T> extends IBaseView<T> {
      * 显示网络错误
      */
     void onShowNetError();
-
-    /**
-     * 设置 presenter
-     */
-    void setPresenter(T presenter);
 
     /**
      * 设置适配器
--- app/src/main/java/com/sqw/mvp_traditional/model/base/IBaseView.java
+++ app/src/main/java/com/sqw/mvp_traditional/model/base/IBaseView.java
@@ -3,26 +3,9 @@ package com.sqw.mvp_traditional.model.base;
 
 /**
  * View 视图接口基类
- * 主要用来定义 界面类的一些通用的UI操作
  */
 
 public interface IBaseView<T> {
-
-    /**
-     * 显示下拉刷新加载动画
-     */
-    void onShowLoading();
-
-    /**
-     * 隐藏下拉刷新加载动画
-     */
-    void onHideLoading();
-
-    /**
-     * 显示网络错误
-     */
-    void onShowNetError();
-
     /**
      * 设置 presenter
      */
